c26b25aff7dc5f1aa5f092131a2c3780e84632c1
Kevin yang <yangke@umich.edu>
2017-05-05 23:56:23 -0400

    install/uninstall dots with stow

--- install.sh
+++ install.sh
@@ -1,31 +1,56 @@
 #!/bin/bash
 
-###################### install.sh ############################
-#
-# 1. Places old dotfiles in ~/old_dotfiles
-# 2. Establishes symlinks from ~/.dotfiles to ~/
-#
-##############################################################
+# Check if GNU stow is installed
+dpkg -s stow > /dev/null 2>&1
+STOW_INSTALLED=$?
 
-############## Variables
+# Uninstall
+if [[ $1 = "-u" ]] || [[ $1 = "--uninstall" ]]; then
+    if [[ "${STOW_INSTALLED}" -ne "0" ]]; then
+        echo "You're SOL. Gonna have to remove the symlinks by hand"
+    else
+        for SUB_MOD in $(ls -d */)
+        do
+            TEMP=${SUB_MOD%/}
+            ( stow -D ${TEMP} > /dev/null 2>&1 )
+            RM_STATUS=$?
+            if [[ "${RM_STATUS}" -ne "0" ]]; then
+                echo "Failed to remove ... ${TEMP}"
+            fi
+        done
+    fi
+# Help
+elif [[ $1 = "-h" ]] || [[ $1 = "--help" ]]; then
+    echo -e "Description: Adds symlinks for all submodules in the ~/.dotfiles directory"
+    echo -e "Options:"
+    echo -e "-h, --help\tDisplay this help menu"
+    echo -e "-u, --uninstall\tRemove symlinks from $HOME"
+# Install
+else
+    if [[ "${STOW_INSTALLED}" -eq "0" ]]; then
+        for SUB_MOD in $(ls -d */)
+        do
+            TEMP=${SUB_MOD%/}
+            echo -en "Install ${TEMP} (y/n)?"
+            read USERINPUT
+            case $USERINPUT in
+                y)
+                    ( stow -R ${TEMP} )
+                    ;;
+                *)
+                    echo "Skipping ${TEMP}"
+                    ;;
+            esac
+        done
+    fi
+fi
 
-dir=~/.dotfiles				# new dotfiles
-olddir=~/.dotfiles_old		# old dotfiles backup
-files=".vimrc .gitconfig .gdbinit"
 
-# Create dotfiles_old directory
-echo "Creating $olddir for backup of dotfiles in ~ that will be overwritten"
-mkdir -p $olddir
-echo "...done"
 
-# Change to .dotfiles directory
-cd $dir
 
-# Move existing dotfiles to olddir, and create symlinks
-for file in $files
-do
-	mv ~/$file $olddir
-	ln -s $dir/$file ~/$file
-done
 
-# source ~/.bashrc
+
+
+
+
+# Stow commands
