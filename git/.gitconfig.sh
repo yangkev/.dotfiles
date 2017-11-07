2549fabc10de674e37c8bc32669d67a5af6cf27b
Kevin Yang <yangke@umich.edu>
2017-11-06 21:14:04 -0500

    Fix check for git bash completion file

--- git/.gitconfig.sh
+++ git/.gitconfig.sh
@@ -1,5 +1,9 @@
 # git bash completion
-if [ -f ~/.git-completion.bash ]; then
+
+if [ -f /etc/bash_completion.d/git ]; then
+    . /etc/bash_completion.d/git
+    __git_complete gch _git_checkout
+elif [ -f ~/.git-completion.bash ]; then
     . ~/.git-completion.bash 
     __git_complete gch _git_checkout
 fi
