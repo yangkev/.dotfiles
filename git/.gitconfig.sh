af505c28adb5452fe32d08fe5e08522f11f471c0
Kevin Yang <yangke@umich.edu>
2018-07-05 21:54:34 -0700

    Fix branch completion for gco

--- git/.gitconfig.sh
+++ git/.gitconfig.sh
@@ -1,10 +1,12 @@
 # git bash completion
 
 if [ -f /etc/bash_completion.d/git ]; then
-    . /etc/bash_completion.d/git
-    __git_complete gch _git_checkout
+  . /etc/bash_completion.d/git
+  __git_complete gch _git_checkout
 elif [ -f ~/.git-completion.bash ]; then
-    . ~/.git-completion.bash 
-    __git_complete gch _git_checkout
+  . ~/.git-completion.bash 
+  __git_complete gco _git_checkout
+  __git_complete gm __git_merge
+  __git_complete gp _git_pull
 fi
 
