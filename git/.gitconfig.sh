e82a52ec8375450388a7d24ecac41a6d04d79ca1
Kevin Yang <yangke@umich.edu>
2018-11-07 22:49:37 -0500

    add more bash completions for git aliases

--- git/.gitconfig.sh
+++ git/.gitconfig.sh
@@ -6,8 +6,11 @@ if [ -f /etc/bash_completion.d/git ]; then
 elif [ -f ~/.git-completion.bash ]; then
   . ~/.git-completion.bash 
   __git_complete gco _git_checkout
-  __git_complete gm __git_merge
+  __git_complete gm _git_merge
   __git_complete gp _git_pull
   __git_complete glg _git_log
+  __git_complete gb _git_branch
+  __git_complete gd _git_diff
+  __git_complete gds _git_diff
 fi
 
