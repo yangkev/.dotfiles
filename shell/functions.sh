e3dbc5e091ea556df013fae77a82f771a1d83d8d
Kevin Yang <yangke@umich.edu>
2020-01-09 22:42:22 -0800

    add pre-commit

--- shell/functions.sh
+++ shell/functions.sh
@@ -1,16 +1,16 @@
 countdown(){
   date1=$((`date +%s` + $1));
-  while [ "$date1" -ge `date +%s` ]; do 
+  while [ "$date1" -ge `date +%s` ]; do
     ## Is this more than 24h away?
     days=$(($(($(( $date1 - $(date +%s))) * 1 ))/86400))
-    echo -ne "$days day(s) and $(date -u --date @$(($date1 - `date +%s`)) +%H:%M:%S)\r"; 
+    echo -ne "$days day(s) and $(date -u --date @$(($date1 - `date +%s`)) +%H:%M:%S)\r";
     sleep 0.1
   done
 }
 
 stopwatch(){
-  date1=`date +%s`; 
-  while true; do 
+  date1=`date +%s`;
+  while true; do
     days=$(( $(($(date +%s) - date1)) / 86400 ))
     echo -ne "$days day(s) and $(date -u --date @$((`date +%s` - $date1)) +%H:%M:%S)\r";
     sleep 0.1
@@ -20,7 +20,7 @@ stopwatch(){
 # Create a new directory in the GOPATH and symlink it to $HOME/src
 godir(){
   dirpath="$GOPATH/src/github.com/yangkev/${1}"
-  ln -s "${dirpath}" "$HOME/src" > /dev/null 2>&1 
+  ln -s "${dirpath}" "$HOME/src" > /dev/null 2>&1
   if [[ $? -ne 0 ]]; then
     echo "symlink already exists. can't create $HOME/src/$1"
     return 1
--- vim/.vim/after/ftplugin/make.vim
+++ vim/.vim/after/ftplugin/make.vim
@@ -1,4 +1,3 @@
 set noexpandtab
 set tabstop=8
 set shiftwidth=8
-
