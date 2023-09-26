90bbdf188d4f1b63bc8f9c6d23ba95b7231dfbc6
Kevin Yang <yangke@umich.edu>
2023-09-26 15:46:10 -0700

    port over all settings from vimrc except lsp

--- nvim/.config/nvim/lua/custom/init.lua
+++ nvim/.config/nvim/lua/custom/init.lua
@@ -1,7 +1,17 @@
--- local autocmd = vim.api.nvim_create_autocmd
+local opt = vim.opt
 
--- Auto resize panes when resizing nvim window
--- autocmd("VimResized", {
---   pattern = "*",
---   command = "tabdo wincmd =",
--- })
+opt.tabstop = 4
+opt.shiftwidth = 4
+opt.softtabstop = 4
+opt.expandtab = true
+opt.smarttab = true
+opt.autoindent = true
+opt.shiftround = true
+opt.smartindent = false
+opt.showmatch = true
+opt.incsearch = true
+opt.relativenumber = true
+opt.wrap = false
+opt.ttimeoutlen = 10
+opt.lazyredraw = true
+opt.wildignore = "*.o,*~,*.pyc,*/.git/*,*/.hg/*,*/.svn/*,*/.DS_Store,*.aux,*.fdb_latexmk,*.fls,*.gz"
--- nvim/.config/nvim/lua/custom/mappings.lua
+++ nvim/.config/nvim/lua/custom/mappings.lua
@@ -1,15 +1,116 @@
 ---@type MappingsTable
 local M = {}
+M.disabled = {
+    i = {
+        ["<C-b>"] = "",
+        ["<C-e>"] = "",
+        ["<C-h>"] = "",
+        ["<C-l>"] = "",
+        ["<C-j>"] = "",
+        ["<C-k>"] = "",
+    },
+    n = {
+        ["<leader>ff"] = "",
+        ["<leader>fb"] = "",
+        ["<leader>fo"] = "",
+        ["<C-s>"] = "",
+        ["<C-c>"] = "",
+        ["<leader>b"] = "",
+        ["<tab>"] = "",
+        ["<S-tab>"] = "",
+        ["<leader>x"] = "",
+    },
+}
 
 M.general = {
     n = {
         [";"] = { ":", "enter command mode", opts = { nowait = true } },
+        ["<leader>cd"] = { "<cmd> cd %:h <CR> :pwd <CR>", "Change cwd to current file" },
     },
     v = {
         [">"] = { ">gv", "indent" },
     },
 }
 
--- more keybinds!
+M.telescope = {
+    plugin = true,
+
+    n = {
+        ["<C-p>"] = { "<cmd> Telescope live_grep <CR>", "Live grep" },
+        ["<leader>t"] = { "<cmd> Telescope <CR>", "Open Telescope" },
+        ["<leader>zf"] = { "<cmd> Telescope find_files <CR>", "Files" },
+        ["<leader>zb"] = { "<cmd> Telescope buffers <CR>", "Buffers" },
+        ["<leader>bz"] = { "<cmd> Telescope current_buffer_fuzzy_find <CR>", "Find in current buffer" },
+        ["<leader>zo"] = { "<cmd> Telescope vim_options <CR>", "Vim Options" },
+        ["<leader>ju"] = { "<cmd> Telescope jumplist <CR>", "Jumplist" },
+    },
+}
+
+M.nvimtree = {
+    plugin = true,
+    n = {
+        ["<leader>nf"] = { "<cmd> NvimTreeFindFile <CR>", "Find current file in nvimtree" },
+    },
+}
+
+M.gitsigns = {
+    n = {
+        ["]h"] = {
+            function()
+                if vim.wo.diff then
+                    return "]c"
+                end
+                vim.schedule(function()
+                    require("gitsigns").next_hunk()
+                end)
+                return "<Ignore>"
+            end,
+            "Jump to next hunk",
+            opts = { expr = true },
+        },
+
+        ["[h"] = {
+            function()
+                if vim.wo.diff then
+                    return "[c"
+                end
+                vim.schedule(function()
+                    require("gitsigns").prev_hunk()
+                end)
+                return "<Ignore>"
+            end,
+            "Jump to prev hunk",
+            opts = { expr = true },
+        },
+
+        ["ghu"] = {
+            function()
+                require("gitsigns").reset_hunk()
+            end,
+            "Reset hunk",
+        },
+
+        ["ghp"] = {
+            function()
+                require("gitsigns").preview_hunk()
+            end,
+            "Preview hunk",
+        },
+
+        ["ghs"] = {
+            function()
+                require("gitsigns").stage_hunk()
+            end,
+            "Stage hunk",
+        },
+
+        ["gb"] = {
+            function()
+                package.loaded.gitsigns.blame_line()
+            end,
+            "Blame line",
+        },
+    },
+}
 
 return M
