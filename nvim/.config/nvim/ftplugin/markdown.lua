8e93e7d4c3d17a648016225f139032b2c152237b
Kevin Yang <yangke@umich.edu>
2023-10-05 23:29:58 -0700

    add markdown ft settings

---
+++ nvim/.config/nvim/ftplugin/markdown.lua
@@ -0,0 +1,11 @@
+-- Vimwiki fi.etype specific settings, even though my vimwiki is in markdown
+local opt_local = vim.opt_local
+opt_local.wrap = true
+opt_local.linebreak = true
+opt_local.list = false
+
+opt_local.relativenumber = false
+opt_local.number = false
+opt_local.signcolumn = "no"
+
+vim.diagnostic.disable()
