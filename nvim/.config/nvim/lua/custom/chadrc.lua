3cd7a0e165af0be5ceabc599da5b509f2d6624b2
Kevin Yang <yangke@umich.edu>
2023-10-11 12:06:16 -0700

    enable netrw so :GBrowse works

--- nvim/.config/nvim/lua/custom/chadrc.lua
+++ nvim/.config/nvim/lua/custom/chadrc.lua
@@ -5,8 +5,8 @@ local M = {}
 local highlights = require("custom.highlights")
 
 M.ui = {
-    theme = "gruvchad",
-    theme_toggle = { "gruvchad", "one_light" },
+    theme = "everforest",
+    theme_toggle = { "everforest", "one_light" },
 
     hl_override = highlights.override,
     hl_add = highlights.add,
@@ -20,5 +20,17 @@ M.plugins = "custom.plugins"
 
 -- check core.mappings for table structure
 M.mappings = require("custom.mappings")
+
+-- Enable netrw for :GBrowse
+-- https://github.com/NvChad/NvChad/issues/2032#issuecomment-1551002296
+M.lazy_nvim = {
+    performance = {
+        rtp = {
+            disabled_plugins = vim.tbl_filter(function(name)
+                return string.sub(name, 1, 5) ~= "netrw"
+            end, require("plugins.configs.lazy_nvim").performance.rtp.disabled_plugins),
+        },
+    },
+}
 
 return M
